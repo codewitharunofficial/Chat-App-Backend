2f8b334e23e400fb90ca612abbb9b273e277a646
Arun <133745182+codewitharunofficial@users.noreply.github.com>
2024-01-05 08:39:44 +0530

    Update mediaController.js

--- Controllers/mediaController.js
+++ Controllers/mediaController.js
@@ -1,7 +1,7 @@
 import userModel from "../Models/userModel.js";
 import cloudinary from "../Helpers/cloudinary.js";
 import ConversationModel from "../Models/ConversationModel.js";
-import ChatAttachmentModel from '../Models/ChatAttachmentsModel.js';
+import ChatAttachmentModel from "../Models/ChatAttachmentsModel.js";
 import path from "path";
 import ChatModel from "../Models/ChatModel.js";
 export const uploadProfilePicture = async (req, res) => {
@@ -18,20 +18,25 @@ export const uploadProfilePicture = async (req, res) => {
         throw new Error("Photo Is Required");
     }
 
-    const user = await userModel.findByIdAndUpdate(req.params.id, {
-      profilePhoto: result,
-    }, {new: true});
+    const user = await userModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        profilePhoto: result,
+      },
+      { new: true }
+    );
 
     await user.save();
 
     const updatedPhotoIfSender = await ConversationModel.updateMany(
       { senderId: req.params.id },
-      { sender: user}, {new: true}
+      { sender: user },
+      { new: true }
     );
 
     const updatePhotoIfReciever = await ConversationModel.updateMany(
       { receiverId: req.params.id },
-      { receiver: user},
+      { receiver: user },
       { new: true }
     );
     res.status(200).send({
@@ -82,67 +87,87 @@ export const sendPhoto = async (req, res) => {
     const { id } = req.params;
     const { photo } = req.files;
 
-    switch(true) {
-        case !id: throw new Error("Id is required");
-        case !photo: throw new Error("Photo is required");
+    switch (true) {
+      case !id:
+        throw new Error("Id is required");
+      case !photo:
+        throw new Error("Photo is required");
     }
 
     const result = await cloudinary.uploader.upload(photo.path, {
-        public_id: `${id}_image`,
+      public_id: `${id}_image`,
     });
 
-    switch(true) {
-        case !result: throw new Error("Photo is required");
+    switch (true) {
+      case !result:
+        throw new Error("Photo is required");
     }
-   
-    const attachmentImage = new ChatAttachmentModel({image: result, senderId: id}).save();
-    res.status(200).send({
-        success: true,
-        message: 'Attachment Sent Successfully',
-        attachmentImage,
-    });
 
+    const attachmentImage = new ChatAttachmentModel({
+      image: result,
+      senderId: id,
+    }).save();
+    res.status(200).send({
+      success: true,
+      message: "Attachment Sent Successfully",
+      attachmentImage,
+    });
   } catch (error) {
     res.status(400).send({
-        success: false,
-        message: 'Something went Wrong',
-        error: error.message
-    })
+      success: false,
+      message: "Something went Wrong",
+      error: error.message,
+    });
   }
 };
 
 export const sendVoiceMessage = async (req, res) => {
   console.log(req.fields, req.files);
   try {
-    const {sender, receiver} = req.fields;
-  const {audio} = req.files;
+    const { sender, receiver } = req.fields;
+    const { audio } = req.files;
 
-  switch(true) {
-    case !sender: throw new Error("Sender Is  required");
-    case !receiver: throw new Error("Receiver Is  required");
-    case !audio: throw new Error("No Audio Found");
-  }
+    switch (true) {
+      case !sender:
+        throw new Error("Sender Is  required");
+      case !receiver:
+        throw new Error("Receiver Is  required");
+      case !audio:
+        throw new Error("No Audio Found");
+    }
 
-  const result = await cloudinary.uploader.upload(audio.path, {
-    public_id: `${sender}_voice`,
-    resource_type: 'auto'
-  })
+    const result = await cloudinary.uploader.upload(audio.path, {
+      public_id: `${sender}_voice`,
+      resource_type: "auto",
+    });
 
-  const voice = new ChatAttachmentModel({senderId: sender, recieverId: receiver, audio: result}).save();
- 
-  const voiceMessage = new ChatModel({sender: sender, reciever: receiver, message: result}).save();
+    const voice = new ChatAttachmentModel({
+      senderId: sender,
+      recieverId: receiver,
+      audio: result,
+    }).save();
 
-  res.status(200).send({
-    success: true,
-    message: "Voice Message Sent Successfully",
-    voiceMessage
-  });
+    const voiceMessage = new ChatModel({
+      sender: sender,
+      reciever: receiver,
+      message: result,
+    }).save();
+    const chat = await ConversationModel.findOneAndUpdate({$or:[{senderId: sender, receiverId: receiver}, {senderId: receiver, receiverId: sender}]}, {chat: voiceMessage}, {new: true}).sort({
+      createdAt: -1,
+    });
+    await chat.save();
+
+    res.status(200).send({
+      success: true,
+      message: "Voice Message Sent Successfully",
+      voiceMessage,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(400).send({
       success: false,
       message: "Something Went Wrong",
-      error: error.message
-    })
+      error: error.message,
+    });
   }
-}
+};
